ab2bf305976a54c80a4bc0fdb32796f076d9eb61
vm75 <vm75.dev@gmail.com>
2023-03-10 08:56:03 -0800

    adding flutter tests. but UTs are not working yet with FFI

---
+++ flutter/test/inditrans_test.dart
@@ -0,0 +1,32 @@
+// ignore_for_file: avoid_print
+
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:test/test.dart';
+import 'package:inditrans/inditrans.dart' as inditrans;
+
+// accept args
+void main() async {
+  await inditrans.init();
+
+  // read json file
+  var json =
+      jsonDecode(File("../test-files/test-cases.json").readAsStringSync());
+
+  // iterate over json which is in the form as in the file test-cases.json
+  for (final input in json) {
+    final description = input['description'];
+    final inputText = input['text'];
+    final inputScript = input['script'];
+    for (final target in input['targets']) {
+      final targetScript = target['script'];
+      final expected = target['text'];
+      test(description, () {
+        final actual =
+            inditrans.transliterate(inputText, inputScript, targetScript);
+        expect(actual, expected);
+      });
+    }
+  }
+}
